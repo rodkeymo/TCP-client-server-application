18617bc696fc3bbff280b15a791547a8cbffe9a5
rodney <52521814+rodkeymo@users.noreply.github.com>
2021-11-11 15:30:18 +0300

    Add files via upload

---
+++ Server.py
@@ -0,0 +1,26 @@
+from socket import *
+import uuid
+
+serverPort = 12010
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(('', serverPort))
+serverSocket.listen(5)
+
+print("Server Started...")
+print("Waiting for other connection")
+
+conn_socket, addr=serverSocket.accept()
+print("Connection established, receiving data...")
+
+f=open(str(uuid.uuid4()),'wb')
+l=conn_socket.recv(1024)
+
+while True:
+	f.write(l)
+	l=conn_socket.recv(1024)
+	if len(l)==0:
+		break
+f.close()
+
+print("All data received, Connection close")
+conn_socket.close()
